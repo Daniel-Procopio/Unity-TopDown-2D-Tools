ee1a22de25c66b5173d9d3dc79622725e1df76bb
Dan <Danielprocopio@ymail.com>
2020-09-27 06:40:03 -0400

    9/27/20 Added Tools, Clock Manager, and RandomMovement.cs
    Tools needs to be re-organized by section
    
    ClockManager simulates a AM/PM Clock
    
    RandomMovement gives a random movement in a certain direction simulating an arc'd curve

---
+++ MyTools/MyClockManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Simulates an AM/PM clock system starting at StartTimeHour/Minute where display minutes increase in increments of 10
+public class MyClockManager : MonoBehaviour
+{
+    private int militaryTimeMinute = 0;
+    private int militaryTimeHour = 0;
+
+    private int displayHour = 0; //AM/PM hour
+    private int displayMinute = 0; //multiples of 10 minutes
+
+    private bool isAM;
+
+    private float tickTimer = 0; //ticks to determine a second in real time using delta time
+    public float realSecondsPerGameMin; //real seconds per in game minute
+
+    public bool timePaused;
+    public bool currentlyIndoors; //if indoors, game time does not proceed
+
+    public int startTimeHour;
+    public int startTimeMinute;
+
+
+    private void Start()
+    {
+        Inititialize();
+    }
+
+
+    private void Update()
+    {
+        if (currentlyIndoors)
+        {
+            //Indoors so we don't pass time
+        }
+
+        else
+        {
+            if (!timePaused)
+            {
+                tickTimer += Time.deltaTime; //change in time
+
+                if (tickTimer >= realSecondsPerGameMin)
+                {
+                    MinutePassed();
+                }
+            }
+        }
+    }
+
+
+    public void Inititialize()
+    {
+        isAM = true;
+
+        militaryTimeHour = startTimeHour;
+        militaryTimeMinute = startTimeMinute;
+        displayHour = startTimeHour;
+        displayMinute = startTimeMinute;
+    }
+
+
+    public void MinutePassed()
+    {
+        militaryTimeMinute += 1;
+        tickTimer -= realSecondsPerGameMin; //Reset tickTimer so it can start counting to 1 
+
+        if(militaryTimeMinute%10 == 0) //only change display minutes if its a multiple of 10
+        {
+            displayMinute = militaryTimeMinute;
+        }
+
+        if(militaryTimeMinute >= 60)
+        {
+            HourPassed();
+        }
+    }
+
+
+    private void HourPassed()
+    {
+        militaryTimeHour++;
+        displayHour++;
+        militaryTimeMinute -= 60;
+        displayMinute -= 60;
+
+        if (displayHour == 12) //if its 12 we change AM/PM
+        {
+            isAM = !isAM;
+        }
+
+        if (displayHour >= 13) //Passed Noon/Midnight
+        {
+            displayHour -= 12;
+        }
+
+        if(militaryTimeHour >= 25)
+        {
+            militaryTimeHour -= 24;
+        }
+    }
+
+
+    public void Pausetime()
+    {
+        timePaused = true;
+    }
+
+
+    public void UnPauseTime()
+    {
+        timePaused = false;
+    }
+
+
+    public void EnteredIndoors()
+    {
+        currentlyIndoors = true;
+    }
+
+
+    public void ExitedIndoors()
+    {
+        currentlyIndoors = false;
+    }
+}
---
+++ MyTools/RandomDirectionMovement.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Requires RigidBody2D on object, set to Kinematic with simulated physics
+
+//This simulates an up and down arcing movement towards a randomized drop direction
+//An arcVector moves perpendicular to the initial direction in order to simulate a rise and fall 
+//The arcVector determines perpendicular angle based on the angle of the initial dropDirectionVector, 
+//Set the dropTimeDelay to delay movement
+//Set lower and upper bound to randomize how far and long movement goes
+//Set lower and upper bound of directionDegrees to determine which direction object can move
+//Set dropSpeed and arcRisingSpeed
+public class RandomDirectionMovement: MonoBehaviour
+{
+    private Rigidbody2D pickerRb;
+
+    private bool currentlyMoving = false;
+    private bool isRising = false;
+
+    public float dropTimeDelay; //delay before movement begins
+
+    public float dropTime; //randomized time it will travel through the air, determines how far something actually goes
+    public float randomDropTimeLowerBound; //random drop time between lower and upper bound
+    public float randomDropTimeUpperBound;
+
+    private float timeSinceStarted;
+    private float percentMovementComplete;
+
+    public float dropDirectionDegrees; //degrees used to determine drop direction vector
+    public int dropDirectionDegreesLowerBound; //can drop between lower and upper bound degrees
+    public int dropDirectionDegreesUpperBound;
+
+    private Vector2 rbCurrentVelocity;
+    private Vector2 dropDirectionVector;
+
+    public float dropSpeed; //speed at which dropped items travel
+    private Vector2 arcVector; //Perpendicular to the drop direction to simulate an arc in movement
+    public float arcRisingSpeed; //speed at which it arcs up and back down
+
+
+    /// <summary>
+    /// On Start we initialize our item picker
+    /// </summary>
+    public virtual void Start()
+    {
+        Invoke("StartDropping", dropTimeDelay);
+    }
+
+    
+    //ran each physics update frame
+    private void FixedUpdate()
+    {
+        if (currentlyMoving)
+        {
+            timeSinceStarted = Time.time;
+            percentMovementComplete = timeSinceStarted / (dropTime / 2); //set to half the dropTime to determine how long it takes to reach apex of arc
+
+            if (isRising == true) //first half of arc of movement
+            {
+                if (percentMovementComplete < 1) //not finished rising
+                {
+                    rbCurrentVelocity = Vector2.Lerp(dropDirectionVector, (dropDirectionVector + arcVector), percentMovementComplete);
+                    pickerRb.velocity = rbCurrentVelocity;
+                }
+
+                else //finished rising
+                {
+                    isRising = false;
+                }
+            }
+
+            else //second half of arc of movement
+            {
+                if (percentMovementComplete < 1) //falling
+                {
+                    rbCurrentVelocity = Vector2.Lerp(dropDirectionVector, (dropDirectionVector - arcVector), percentMovementComplete);
+                    pickerRb.velocity = rbCurrentVelocity;
+                }
+
+                else //Completed the drop
+                {
+                    pickerRb.velocity = Vector2.zero;
+                }
+            }
+        }
+    }
+
+
+    //dropDirectionDegrees determines direction of arcVector 
+    public void StartDropping()
+    {
+        pickerRb = transform.GetComponent<Rigidbody2D>();
+
+        dropDirectionDegrees = UnityEngine.Random.Range(dropDirectionDegreesLowerBound, dropDirectionDegreesUpperBound);
+
+        dropTime = UnityEngine.Random.Range(randomDropTimeLowerBound, randomDropTimeUpperBound); //How long it will drop randomized between two values
+
+        dropDirectionVector = DegreeToVector2(dropDirectionDegrees) * dropSpeed;
+
+        //if trajectory on right side of player arcVector + 90f degrees
+        if ((dropDirectionDegrees >= 0 && dropDirectionDegrees <= 90) || (dropDirectionDegrees > 270 && dropDirectionDegrees < 360))
+        {
+            arcVector = DegreeToVector2(dropDirectionDegrees + 90f) * arcRisingSpeed;
+        }
+
+        else //if trajectory on left side of player arcVector - 90f degrees
+        {
+            arcVector = DegreeToVector2(dropDirectionDegrees - 90f) * arcRisingSpeed;
+        }
+
+        currentlyMoving = true;
+        isRising = true;
+    }
+
+
+    //converts radians to direction Vector2 
+    public static Vector2 RadianToVector2(float radian)
+    {
+        return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
+    }
+
+    //converts degrees to direction Vector2
+    public static Vector2 DegreeToVector2(float degree)
+    {
+        return RadianToVector2(degree * Mathf.Deg2Rad);
+    }
+}
